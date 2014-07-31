f8441c6888264a59043c61eb4b99d06c568ad27c
Pieter van de Bruggen <pvande@gmail.com>
2014-07-31 16:44:44 -0700

    Removing a dependency on an absent Homebrew package.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,6 @@ class baseline {
   $users = split($user_accounts, ':')
   baseline::rcfiles { $users: group => 'staff' }
 
-  # Install a fresher SSL CA bundle.  Tools that would otherwise use the system
-  # CA bundle and fail (like Ruby's `open-uri`) can now properly verify a much
-  # broader range of SSL certificates.
-  package { 'curl-ca-bundle':
-    ensure   => installed,
-    provider => 'homebrew',
-  }
 
   # Since DropPod relies heavily on Git, there's no sense in us preferring to
   # use whatever stale version ships with XCode.
